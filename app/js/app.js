f069a71cc21064f23fbd8e8dfa35522a351fb564
ganouri <guillaume.ang@gmail.com>
2013-04-04 15:01:33 +1100

    Some modifs to Temp/

--- app/js/app.js
+++ app/js/app.js
@@ -19,17 +19,6 @@ edgeBudModule.config(['$routeProvider', function($routeProvider) {
 	  	  when('/projects', {templateUrl: 'views/projects.html'}).
 	  	  when('/projects/new', {templateUrl: 'views/add-project.html', controller: "ProjectController"}).
 	  	  when('/projects/:id', {templateUrl: 'views/project-details.html', controller: "ProjectDetailsController"}).
+	      when('/temp/', {templateUrl: 'views/project-live.html'}).
 	      otherwise({redirectTo: '/projects'});
 	}]);
-
-window.blabla = function() {
-	$(".challenge_inline[data-title]").each(function(i, elem) {
-		var text = "";
-		var timer = setInterval(function() {
-		  text = $(elem).attr("data-title").substr(0,text.length+1);
-		  $(elem).html(text);
-		  if(text.length === $(elem).attr("data-title").length)
-		    clearInterval(timer);
-		}, 50);
-	});
-};
